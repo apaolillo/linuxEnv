7e6a9c92db0bb171ecbb175d7620968fc3add1bc
Antonio Paolillo <antonio.paolillo@ulb.ac.be>
2014-04-27 17:08:27 +0200

    In deploy script, add configuration for HIPPEROS workstation.

--- setupLinuxWorkstationEnv.sh
+++ setupLinuxWorkstationEnv.sh
@@ -8,13 +8,26 @@ ALL_PACKAGES=vim git
 DEVENV_REPO_DIR=.
 CONFIGFILES_DIR=$DEVENV_REPO_DIR/configFiles
 
+# Check for SSH key
 if [ ! -f ~/.ssh/id_rsa ]; then
     echo "There is no ssh key defined in ~/.ssh\nPlease use the following command:\n  ssh-keygen -t rsa -C \"$EMAIL\""
     exit -1
 fi
 
-cp $CONFIGFILES_DIR/.gitconfig ~/.gitconfig
+# Configure git
+cp $CONFIGFILES_DIR/.gitconfig $HOME/.gitconfig
 
+# Install main packages
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends $ALL_PACKAGES
+
+# Configure machine for HIPPEROS
+GIT_REPOS=$HOME/git
+returnDir=`pwd`
+mkdir -p $GIT_REPOS
+cd $GIT_REPOS
+git clone ssh://git@www.itial.be:2002/hipperosDevEnv
+cd $GIT_REPOS/hipperosDevEnv
+bash setupHipperosDevEnv.sh
+cd $returnDir
 
