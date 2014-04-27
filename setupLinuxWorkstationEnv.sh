2d2661a52c61b126154f96f0ddbad33632ac44b6
Antonio Paolillo <antonio.paolillo@ulb.ac.be>
2014-04-27 17:02:19 +0200

    Add the configuration of git in the devEnv script.

--- setupLinuxWorkstationEnv.sh
+++ setupLinuxWorkstationEnv.sh
@@ -5,11 +5,15 @@ set -e
 
 EMAIL=antonio.paolillo@ulb.ac.be
 ALL_PACKAGES=vim git
+DEVENV_REPO_DIR=.
+CONFIGFILES_DIR=$DEVENV_REPO_DIR/configFiles
 
 if [ ! -f ~/.ssh/id_rsa ]; then
     echo "There is no ssh key defined in ~/.ssh\nPlease use the following command:\n  ssh-keygen -t rsa -C \"$EMAIL\""
     exit -1
 fi
+
+cp $CONFIGFILES_DIR/.gitconfig ~/.gitconfig
 
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends $ALL_PACKAGES
