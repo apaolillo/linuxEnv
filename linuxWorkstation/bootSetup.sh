c4022dbb95ca3c10c4b58abb4e74e31b661bbb55
Antonio Paolillo <antonio.paolillo@ulb.ac.be>
2014-08-05 15:02:24 +0200

    [Linux automated deploy] Add script for init steps

---
+++ linuxWorkstation/bootSetup.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# This script provides the first commands to execute when installing a new machine used as 'workstation'.
+
+ssh-keygen -t rsa -C "antonio.paolillo@ulb.ac.be"
+
+sudo apt-get update 
+sudo apt-get install -y git
+git config --global user.name "Antonio Paolillo"
+git config --global user.email "antonio.paolillo@ulb.ac.be"
+mkdir -p ~/git
+
+echo "You must now provide the new SSH public key to the Github admin panel
+(https://github.com/settings/ssh) in order to have access to private
+repositories. After that, you will be able to launch deploy scripts.
+
+The public key is the following:
+"
+cat ~/.ssh/id_rsa.pub
+
